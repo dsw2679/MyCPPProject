76a06af0ee98f104134ff757a9d8b7fe9562fc1d
김동석 <dsw2679@naver.com>
2026-01-16 18:54:01 +0900

    [01.16] 플레이어애니메이션 리타기팅, animinstance, locomotion

--- MyCPPProject.cpp
+++
@@ -1,9 +0,0 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
-#include "MyCPPProject.h"
-#include "Modules/ModuleManager.h"
-
-IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, MyCPPProject, "MyCPPProject" );
-
-DEFINE_LOG_CATEGORY(LogMyCPPProject)
- 
--- Private/Experience/MyPawnData.cpp
+++
@@ -1,9 +0,0 @@
-// Fill out your copyright notice in the Description page of Project Settings.
-
-
-#include "Experience/MyPawnData.h"
-
-UMyPawnData::UMyPawnData()
-{
-	// 필요시 초기화 코드 작성
-}
